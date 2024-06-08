2ab3488ba116bc5b357a97fb09fa3cb1d22522e8
melatech <maincontractor@gmail.com>
2024-06-08 17:28:11 +0100

    Init commit v4

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+  # these methods are called (actions) a of the pages controller
+  def home
+  end
+
+  def about
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  # we are setting the action of the controller to go to pages folder and find the home html 
+  # substitute the "get" for the url for example (http://localhost:3000/about) the slash then the route 'name'
+
+  # get 'pages/home'
+  # get 'home', to: 'pages#home'
+
+  # if i want to link to the about path i have to use the "about_path" variable string
+
+  get 'about', to: 'pages#about'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # Go into the (page controller)and look for an home action)
+   root "pages#home"
+end
